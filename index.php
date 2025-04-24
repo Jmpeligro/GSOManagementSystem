452299bdfc0adbb9320bd1915f29ec3f5d68a66d
Jmpeligro <peligro_johnmichael@plpasig.edu.ph>
2025-04-24 11:17:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 session_start();
 
 if (isset($_SESSION['user_id'])) {
+    $user_name = $_SESSION['first_name'] . ' ' . $_SESSION['last_name'];
+    echo "<p>Welcome, $user_name!</p>";
     header("Location: dashboard.php");
     exit();
 } else {
