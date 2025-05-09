03b7972d4e54c59ef96f1c4c3e620413f10a6e4d
DomCM <macuana_arnolddominic@plpasig.edu.ph>
2025-05-09 13:21:29 +0800

    Update RemindDue.php
    - Revised code to ensure push notification is in line with success/failure to send email via return boolean from notifications.php.
    - Restructured email body. Added more information.
    - Verbose push notification.
    - Added function getBorrowedDueDate(). Not sure if a similar function already exists in other classes.
    - Email's alternative text / non-html alternative still needs to be updated.

--- php/borrowings/RemindDue.php
+++ php/borrowings/RemindDue.php
@@ -2,7 +2,8 @@
     require_once 'notifications.php';
 
     if (isset($_GET['id'])) {
-        $equipmentID = $_GET['id'];
+        $borrowing_ID = $_GET['id'];
+        $equipment_ID = $_GET['equipment_id'];
         $user_id = $_GET['user_id'];
     }    
 
@@ -11,20 +12,45 @@
     require_once '../classes/Equipment.php';
     $conn = $db->getConnection();
 
+    function getBorrowedDueDate($borrowingID, $conn) {
+        $sql = "SELECT due_date FROM borrowings WHERE borrowing_id = ? AND status = 'active'";
+        $stmt = $conn->prepare($sql);
+        if ($stmt === false) {
+            error_log('Error preparing statement in getBorrowedCount: ' . $conn->error);
+            return 0;
+        }
+        
+        $stmt->bind_param("i", $borrowingID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc()['due_date'];
+    }
+
     $user = new User($conn);
     $user->load($user_id);
     $email = $user->getEmail();
     $name = $user->getFullName();    
 
     $equipment = new Equipment($conn);
-    $equipment->load($equipmentID);
+    $dueDate = getBorrowedDueDate($borrowing_ID, $conn);
+    $equipment->load($equipment_ID);
     $equipmentName = $equipment->getName();
 
     $emailBody = "
-            <B><U>THIS IS AN AUTOMATICALLY GENERATED EMAIL | DO NOT REPLY</U></B> <BR>
-            <hr>
-            <p>The equipment you borrowed is past its due date, please return it at your soonest convinience. </p>
-            <p>Equipment: '$equipmentName'</p>
+            <p>Dear $name,<br>
+            This email serves as a friendly reminder that the equipment listed below was due for return on $dueDate:</p>
+            Equipment Details:
+            <ul>
+                <li>Equipment Name: $equipmentName</li>
+                <li>Original Due Date: $dueDate</li>
+            </ul>
+            We implore you to return the aforementioned equipment at your earliest convenience.<br><br>
+
+            Thank you for your understanding.<br><br>
+            Sincerely,<br>
+
+            General Service Office
+
         ";
 
         $emailBodyAlt = "
@@ -35,8 +61,13 @@
         
         ";
 
-        pushNotif("Email Sent", "An Email has been sent to remind " . $name . " of their due date at " . $email);
-        sendEmail($email, $name, "Equipment Return Due Date", $emailBody, $emailBodyAlt);
+        
+        $sent = sendEmail($email, $name, "College Project Testing SMTP", $emailBody, $emailBodyAlt);
+        if ($sent === true) {
+            pushNotif("Email Sent", "An Email has been sent to remind " . $name . " of their due date for " . $equipmentName . " on " . $dueDate . " at " . $email);
+        } else {
+            pushNotif("Email Failed to Send", "There was an error sending an email to " . $name . ". Please try again later");
+        }
         echo "<script>history.back();</script>";
         exit();
 ?>
