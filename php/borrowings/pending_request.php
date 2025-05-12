7ca1749245e2de266aa6a19b5eed7a7f1ec9a9f3
DomCM <macuana_arnolddominic@plpasig.edu.ph>
2025-05-13 00:04:09 +0800

    Update pending_request.php
    added function to send email when request approved/denied.
    pushnotif not implemented yet.
    email format still lacking detail.
    need review if logic for sendmail() placement is good.

--- php/borrowings/pending_request.php
+++ php/borrowings/pending_request.php
@@ -1,12 +1,20 @@
 <?php
 session_start();
 require_once '../db_connection.php';
-require_once '../classes/Borrowing.php';
+require_once '../helpers.php'; 
+require_once '../classes/borrowing/Borrowing.php';
+require_once 'notifications.php';
+require_once '../classes/User/UserBase.php';
 
 if (!isLoggedIn() || !isAdmin()) {
     header("Location: php/login.php");
     exit();
 }
+
+$userBase = new UserBase($conn);
+$userBase->load($_SESSION['user_id']);
+$recipientEmail = $userBase->getEmail();
+$name = $userBase->getFullName();   
 
 if (isset($_POST['approve_request'])) {
     $borrowing_id = (int)$_POST['borrowing_id'];
@@ -25,6 +33,18 @@ if (isset($_POST['approve_request'])) {
     }
     
     header("Location: pending_request.php");
+    $emailBody = "
+        <B><U>THIS IS AN AUTOMATICALLY GENERATED EMAIL | DO NOT REPLY</U></B> <BR>
+        <hr>
+        <p>The equipment you requested to borrow has been approved.</p>
+        <p>Please visit the General Service Office at your soonest convenience to collect/claim your requested equipment.</p>
+    ";
+
+    $emailBodyAlt = "
+        text -- fill this up with alternative text :D
+    ";
+    pushNotif("Email Sent", "An Email has been sent notify " . $name . " of their request status at " . $email);
+    sendEmail($recipientEmail, $name, "Borrow Request: Approved", $emailBody, $emailBodyAlt);
     exit();
 }
 
@@ -46,6 +66,17 @@ if (isset($_POST['deny_request'])) {
     }
     
     header("Location: pending_request.php");
+    $emailBody = "
+        <B><U>THIS IS AN AUTOMATICALLY GENERATED EMAIL | DO NOT REPLY</U></B> <BR>
+        <hr>
+        <p>The equipment you requested to borrow has been denied.</p>        
+    ";
+
+    $emailBodyAlt = "
+        text -- fill this up with alternative text :D
+    ";
+    pushNotif("Email Sent", "An Email has been sent notify " . $name . " of their request status at " . $email);
+    sendEmail($recipientEmail, $name, "Borrow Request: Approved", $emailBody, $emailBodyAlt);
     exit();
 }
 
